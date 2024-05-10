abeb4d9c793717afd5f109014f7a68a394c26c29
afzasadaralam <shaikhalam961@gmail.com>
2024-05-10 09:49:02 -0700

    first commit

---
+++ node_modules/code-with-afza-cli-number-guessing/index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/enov node
+import inquirer from 'inquirer';
+console.log("welcom to codewithafza - CLI Number Guessing Game");
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "write your guess b/w 1 to 10:"
+    }
+]);
+const { userGuess } = answers;
+console.log(userGuess, "userGyess", systemGeneratedNo, 'GYs');
+if (userGuess === systemGeneratedNo) {
+    console.log("yup your answer is correct /n you win!");
+}
+else {
+    console.log("please try again!");
+}
